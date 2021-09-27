4794579676feefa8d99045a69addc00d24b9f6c5
fernandamichetti <f.michetti@hotmail.com>
2021-09-27 16:33:06 -0300

    Adicionando lista com conteúdo da API na página

--- script.js
+++ script.js
@@ -15,7 +15,22 @@ const handleSearchEvent = () => {
 const fetchCurrency = () => {
   const saveFetch = fetch(`https://api.exchangerate.host/latest`)
   .then((response) => response.json())
-  .then((object) => console.log(object))
+  .then((object) => {
+    const rates = object.rates;
+    const ratesEntries = Object.entries(rates)
+    // console.log(ratesEntries);
+    ratesEntries.forEach((entry) => {
+      const currency = entry[0];
+      const rates = entry[1];
+      console.log(currency, rates);
+
+      const currencyList = document.querySelector('#currency-list');
+      const itenCurencyList = document.createElement('li');
+      itenCurencyList.innerText = `${currency}: ${rates}`;
+      console.log(itenCurencyList);
+      currencyList.appendChild(itenCurencyList)
+    })
+  })
   .catch((error) => console.log(error, 'Solicitação falhou'))
 }
 
